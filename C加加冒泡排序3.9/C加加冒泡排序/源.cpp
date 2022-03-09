0c425a887e81ccaa45eab3b768657a38910bd278
CodeZeng0214 <3294639642@qq.com>
2022-03-09 20:14:56 +0800

    未完成

---
+++ C加加冒泡排序3.9/C加加冒泡排序/源.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//ц╟ещеепР
+#include <iostream>
+using namespace std;
+
+void Bubble(int a[], int n)
+{
+
+}
+int main()
+{
+	int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int s = sizeof(a) / sizeof(a[0]);
+	Bubble(a[10],s);
+}
